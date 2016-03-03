c095b18756a12cce13bcb36d78d98acb9ccdeec0
am0995 <alyssam0995@tywls.org>
2016-03-03 21:12:20 +0000

    Istructions removed

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
       $(document).ready(function() {
       $("#playButton").click(play)
-        // ****** STEP ONE ******
-        // attach a click event listener here
-        // to your play button (get its ID at the bottom of this page)
-        // and attach it to the play function
       
       });
-      
-      ////////////////////////////////////////////////
-      // these are helper functions, don't touch!
-      ////////////////////////////////////////////////
+
       function getRandomPlay(){
         var outcomeArray = ["Rock", "Paper", "Scissors"];
         return outcomeArray[ Math.floor(Math.random() * outcomeArray.length) ];
@@ -19,10 +12,6 @@
         $(".girl").removeClass("girl-lose"); 
         $(".boy").removeClass("boy-lose");  
       }
-      ////////////////////////////////////////////////
-      // end of helper functions
-      ////////////////////////////////////////////////
-      
       
       function play(){
         
@@ -40,17 +29,6 @@
         
         } else{
 
-          // ****** STEP TWO ******
-          // there are three conditions where the boy could win
-          // 1. if boy played Rock and girl played Scissors
-          // 2. if boy played Paper and girl played Rock
-          // 3. if boy played Scissors and girl played Paper
-          
-          // write an if / if else statements testing for these
-          // conditions. If the boy wins, run the function boyWins(),
-          // If the girl wins, run the function girlWins()
-          // hints: == tests if two things are equal, && means "and"
-          // if (boyPlayed == "Rock" && girlPlayed == "Scissors") {}
           if(boyPlayed == "Rock" && girlPlayed == "Scissors"){
             boyWins();
             
@@ -76,15 +54,13 @@
       }
       
       function boyWins(){
-        // ****** STEP THREE ******
-        // add the class "girl-lose" to the girl div
+
         $(".girl").addClass("girl-lose")
         $("#message").html("Boy wins!");
       }
       
       function girlWins(){
-        // ****** STEP THREE ******
-        // add the class "boy-lose" to the boy div
+
         $(".boy").addClass("boy-lose")
         $("#message").html("Girl wins!");
       }
